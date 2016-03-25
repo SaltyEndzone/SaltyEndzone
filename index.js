9e106a89909a7730ed7375c20742a3441070309e
Justin Bell <justinbell714@gmail.com>
2016-03-24 19:48:51 -0700

    A commit

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './public/App/App'
+import HomePage from './public/home/HomePage'
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <div>
+      < HomePage />
+      </div>
+    )
+  }
+}
 
 render(
   <App />,
