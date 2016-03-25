e3c8344d967bd3c32d28e0c7cd2024ee27d3dd15
Kathleen L <kathleeng.lopez@gmail.com>
2016-03-25 15:38:09 -0700

    Added styling to home page

--- public/App/App.js
+++ public/App/App.js
@@ -5,22 +5,14 @@ import Discussion from '../Discussion/Discussion'
 
 export default class App extends Component {
   render() {
-    return (
-      <div>
-        <div className="menu-bar">
-        <span className="title">DataStructr</span>
-        </div>
-
-        <div className="row">
-          <div className="col-md-6">
-            <LinkedList />
-          </div>
-          <div className="col-md-6">
-            <Discussion />
-          </div>
-        </div>
-      </div>
-
+    return ( 
+    <div className="menu-bar">
+    <span className="title">DataStructr</span>
+    <span className="login">LOGIN</span>
+      <HomePage />
+    </div>
     )
   }
 }
+
+
