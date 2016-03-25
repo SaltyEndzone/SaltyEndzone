5d293c52d9a23f863858ae7c5e765d2192263037
Justin Bell <justinbell714@gmail.com>
2016-03-24 19:57:58 -0700

    Added homepage component

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import HomePage from './public/home/HomePage'
 
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './public/App/App'
-import HomePage from './public/home/HomePage'
 
-class HelloWorld extends Component {
-  render() {
-    return (
-      <div>
-      < HomePage />
-      </div>
-    )
-  }
-}
 
 render(
   <App />,
