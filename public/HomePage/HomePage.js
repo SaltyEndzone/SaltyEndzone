817e792033d42969959f3a86cce0a15a0498e93f
Kathleen L <kathleeng.lopez@gmail.com>
2016-03-26 10:09:23 -0700

    Spacing adjusted

--- public/HomePage/HomePage.js
+++ public/HomePage/HomePage.js
@@ -7,31 +7,14 @@ export default class HomePage extends Component {
   render() {
     return (
       <div>
-    
-        
-      
         <div className="data-buttons-section">
-
-         
           <img className="data-structure-buttons" src ={require('./img/stack.png')} />
-
-          
           <img className="data-structure-buttons" src ={require('./img/queue.png')} />
-
-          
           <img className="data-structure-buttons" src={require('./img/tree.png')} />
-
-          
           <img className="data-structure-buttons" src={require('./img/linkedList.png')} />
-
-          
           <img className="data-structure-buttons" src ={require('./img/graph.png')} />
-
-          
-          <img className="data-structure-buttons" src ={require('./img/hashTable.png')} />
-          
+          <img className="data-structure-buttons" src ={require('./img/hashTable.png')} /> 
         </div>
-
       </div>
     )
   }
