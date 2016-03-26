ae0348851fdd1d664895ff1c349bd2cbe778f966
Justin Bell <justinbell714@gmail.com>
2016-03-25 23:43:46 -0700

    Homepage/App changes

--- public/HomePage/HomePage.js
+++ public/HomePage/HomePage.js
@@ -11,12 +11,24 @@ export default class HomePage extends Component {
         
       
         <div className="data-buttons-section">
+         
           <img className="data-structure-buttons" src ={require('./img/stack.png')} />
+
+          
           <img className="data-structure-buttons" src ={require('./img/queue.png')} />
+
+          
           <img className="data-structure-buttons" src={require('./img/tree.png')} />
+
+          
           <img className="data-structure-buttons" src={require('./img/linkedList.png')} />
+
+          
           <img className="data-structure-buttons" src ={require('./img/graph.png')} />
+
+          
           <img className="data-structure-buttons" src ={require('./img/hashTable.png')} />
+          
         </div>
 
       </div>
