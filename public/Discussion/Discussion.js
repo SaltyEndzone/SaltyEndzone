2dc83e4d1fca8f933b7c7adb72a8710f4f0fa9e7
Kathleen L <kathleeng.lopez@gmail.com>
2016-03-25 17:31:48 -0700

    Editing code section

--- public/Discussion/Discussion.js
+++ public/Discussion/Discussion.js
@@ -6,16 +6,18 @@ export default class Discussion extends Component {
       <div>
         <div>
           <button className="discussion-title">DISCUSSION</button>
-            <p className="discussion">Kathybeatz: How do you traverse through a tree?</p>
-            <p className="discussion">MickyDWow: You can traverse through a tree by recursing over each node's children</p>
+            <p className="comments">Kathybeatz: How do you traverse through a tree?</p>
+            <p className="comments">MickyDWow: You can traverse through a tree by recursing over each node's children</p>
         </div>
 
         <div className="col-md-6 discussion">
-          <input type="text" className="form-control" placeholder="Search for...">
-            <span className="input-group-btn">
-              <button className="btn btn-default" type="button">Go!</button>
-            </span>
-          </input>
+          <div class="input-group">
+            <input type="text" className="form-control" placeholder="Comment here ...">
+              <span className="input-group-btn">
+                <button className="btn btn-default" type="button">POST</button>
+              </span>
+            </input>
+          </div>
         </div>
       </div>
     )
