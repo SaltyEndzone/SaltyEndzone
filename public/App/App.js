b792bc1980601168e22f393d37c51d73867391b6
EBiddlecome <elizabethwbiddlecome@gmail.com>
2016-03-29 12:34:08 -0700

    added tree

--- public/App/App.js
+++ public/App/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react'
+import Discussion from '../Discussion/Discussion.js'
 
-import HomePage from '../HomePage/HomePage'
-import LinkedList from '../LinkedList/LinkedList'
-import Discussion from '../Discussion/Discussion'
-import Code from '../Code/Code'
-
-export default class App extends Component {
+export default class Tree extends Component {
   render() {
-    return ( 
-      <div>
-        <div className="menu-bar">
-          <img className="logo" src="http://visualgo.net/img/heap.gif" />
-          <span className="title">Data.Structrz</span>
+    return (
+
+        <div className="col-md-6">
+          <button className="dataStructureTitle">TREE</button>
+            <img className="tree-image" src="http://blog.hackerearth.com/wp-content/uploads/2015/05/dfsbfs_animation_final.gif" />
+          <div>
+
+          <div className="action-section">
+            <button className="action-title">DEPTH FIRST SEARCH</button>
+            <button className="action-title">BREADTH FIRST SEARCH</button>
+            <button className="action-title">ADD CHILD</button>
+            <button className="action-title">CONTAINS</button>
+          </div>
         </div>
-        <LinkedList />
-      </div>
+        </div>
     )
   }
 }
--- public/LinkedList/LinkedList.js
+++ public/LinkedList/LinkedList.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import Discussion from '../Discussion/Discussion.js'
 
-export default class LinkedList extends Component {
+export default class Tree extends Component {
   render() {
     return (
 
         <div className="col-md-6">
-          <button className="dataStructureTitle">LINKED LIST</button>
-            <img className="linkedlist-image" src="http://visualgo.net/img/list.gif" />
+          <button className="dataStructureTitle">TREE</button>
+            <img className="tree-image" src="http://blog.hackerearth.com/wp-content/uploads/2015/05/dfsbfs_animation_final.gif" />
           <div>
 
           <div className="action-section">
-            <button className="action-title">ADD TO TAIL</button>
-            <button className="action-title">REMOVE HEAD</button>
+            <button className="action-title">DEPTH FIRST SEARCH</button>
+            <button className="action-title">BREADTH FIRST SEARCH</button>
+            <button className="action-title">ADD CHILD</button>
             <button className="action-title">CONTAINS</button>
           </div>
         </div>
