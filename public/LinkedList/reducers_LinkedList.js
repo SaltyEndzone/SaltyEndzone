16cc47e5252c5f1240439ff7714834979a9bbbde
Justin Bell <justinbell714@gmail.com>
2016-04-02 12:02:06 -0700

    Added comments noting changes in version (redux files added)

--- public/LinkedList/reducers_LinkedList.js
+++ public/LinkedList/reducers_LinkedList.js
@@ -1,2 +1,5 @@
 import { ADD_NODE } from './action_LinkedList'
 
+//as it stands the difference between this version
+//and the the master on the fork and remote is that
+//the actions/reducers/code files for linkedlist were added
