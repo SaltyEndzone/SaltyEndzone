e5c7117e0dc3fe86c1350da6b40a724a7df01410
Kathleen L <kathleeng.lopez@gmail.com>
2016-04-03 11:11:29 -0700

    Adding boilerplate code snippets for queue and linked list

---
+++ public/LinkedList/LinkedListCodeSnippets/LinkedListContains.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class LinkedListContains extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
---
+++ public/LinkedList/LinkedListCodeSnippets/LinkedListRemoveHead.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class LinkedListRemoveHead extends Component {
+  render() {
+    return (
+
+    )
+  }
+}
