cca54e22a9444428c283c5003d283d2d86f89097
Kathleen L <kathleeng.lopez@gmail.com>
2016-04-03 12:40:44 -0700

    Filled in code snippets for linked lists

--- public/LinkedList/LinkedListCodeSnippets/LinkedListAddToTail.js
+++ public/LinkedList/LinkedListCodeSnippets/LinkedListAddToTail.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react'
 export default class LinkedListAddToTail extends Component {
   render() {
     return (
-
+      <div>
+        <div>{'list.addToTail = function(value){'}</div>
+          <div className="indent">{'var newTail = Node(value);'}</div>
+          <div className="indent">{'if (!list.head) {'}</div>
+          <div className="double-indent">{'list.head = newTail;'}</div>
+          <div className="indent">{'}'}</div>
+          <div className="indent">{'if (list.tail) {'}</div>
+          <div className="double-indent">{'list.tail.next = newTail;'}</div>
+          <div className="indent">{'}'}</div>
+          <div className="indent">{'list.tail = newTail;'}</div>
+          <div className="indent">{'};'}</div>
+      </div>
     )
   }
 }
--- public/LinkedList/LinkedListCodeSnippets/LinkedListRemoveHead.js
+++ public/LinkedList/LinkedListCodeSnippets/LinkedListRemoveHead.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react'
 export default class LinkedListRemoveHead extends Component {
   render() {
     return (
-
+<div>
+  <div>{'list.removeHead = function(){'}</div>
+  <div className="indent">{'if (list.head === null){'}</div>
+  <div className="double-indent">{'return null;'}</div>
+  <div className="indent">{'}'}</div>
+  <div className="indent">{'var currentHead = list.head;'}</div>
+  <div className="indent">{'list.head = list.head.next;'}</div>
+  <div className="indent">{'return currentHead.value;'}</div>
+  <div>{'};'}</div>
+</div>
     )
   }
 }
