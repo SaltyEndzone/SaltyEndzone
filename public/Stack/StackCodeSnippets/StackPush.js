246c291325c2460bc24527500e3cc452f2a91606
Kathleen L <kathleeng.lopez@gmail.com>
2016-04-03 11:29:32 -0700

    Filled in code snippets for stack

--- public/Stack/StackCodeSnippets/StackPush.js
+++ public/Stack/StackCodeSnippets/StackPush.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react'
 export default class StackPush extends Component {
   render() {
     return (
-
+      <div>
+        <div>{'Stack.prototype.push = function(value){'}</div>
+        <div className="indent">{'this._storage[this._size] = value;'}</div>
+        <div className="indent">{'this._size++;'}</div>
+        <div>{'};'}</div>
+      <div>
     )
   }
 }
