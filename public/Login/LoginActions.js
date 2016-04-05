1ff7d71bac4e3aeacf9d7175a7589443e170509e
Kathleen L <kathleeng.lopez@gmail.com>
2016-04-04 19:22:26 -0700

    Fixed linting for Login

--- public/Login/LoginActions.js
+++ public/Login/LoginActions.js
@@ -1,15 +1,15 @@
-export const OPEN_LOGIN = 'OPEN_LOGIN'
-export const CLOSE_LOGIN = 'CLOSE_LOGIN'
+export const OPEN_LOGIN = 'OPEN_LOGIN';
+export const CLOSE_LOGIN = 'CLOSE_LOGIN';
 
 export function openLogin() {
   return {
     type: OPEN_LOGIN
-  }
+  };
 }
 
 export function closeLogin() {
   return {
     type: CLOSE_LOGIN
-  }
+  };
 }
 
