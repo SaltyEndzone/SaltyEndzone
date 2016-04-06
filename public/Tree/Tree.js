b0187b797e62c27066704eef1c9834769bd1ddee
Kathleen L <kathleeng.lopez@gmail.com>
2016-04-05 18:58:38 -0700

    Added Greensock node modules and testing svg tree animation

--- public/Tree/Tree.js
+++ public/Tree/Tree.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react';
 import TreeAnimation from './TreeAnimation';
 import TreeFunctions from './TreeFunctions';
 
+// <img className="tree-image" src="http://blog.hackerearth.com/wp-content/uploads/2015/05/dfsbfs_animation_final.gif" />
 
 export default class Tree extends Component {
   render() {
     return (
         <div className="col-md-6">
           <button className="dataStructureTitle">TREE</button>
-          <img className="tree-image" src="http://blog.hackerearth.com/wp-content/uploads/2015/05/dfsbfs_animation_final.gif" />
           <div>
-            <TreeAnimation />
+            <TreeAnimation width="550" height="380" />
             <TreeFunctions />
           </div>
         </div>
