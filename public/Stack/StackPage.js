8776f6ede4b8eb0c13fbba242ab1b3cf774c4c23
Justin Bell <justinbell714@gmail.com>
2016-04-11 09:32:44 -0700

    Local authentication strategy works. Separated out discussion components in to relevant containers.

--- public/Stack/StackPage.js
+++ public/Stack/StackPage.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 
-import Stack                from '../Stack/Stack';
-import Discussion           from '../Discussion/Discussion';
-import StackCode          from './StackCode';
-
+import Stack from '../Stack/Stack';
+import StackDiscussion from './StackDiscussion';
+import StackCode from './StackCode';
 
 
 export default class StackPage extends Component {
   render() {
 
-    return ( 
-      <div>
-        <div className="row">
-          <div className="col-md-6">
-            <Stack />
-          </div>
-          <div className="col-md-6">
-            <StackCode />
-            <StackDiscussion />
-          </div>
+    return <div>
+      <div className="row">
+        <div className="col-md-6">
+          <Stack />
+        </div>
+        <div className="col-md-6">
+          <StackCode />
+          <StackDiscussion />
         </div>
       </div>
     </div>
