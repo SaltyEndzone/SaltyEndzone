0f4589db8568f0b3bbd8db3a54e35a5a0f513ad1
Kathleen L <kathleeng.lopez@gmail.com>
2016-04-11 16:32:26 -0700

    Added click event to bottom tree nodes

--- public/Tree/TreeSVG/TreeLvl3RightChild6SVG.js
+++ public/Tree/TreeSVG/TreeLvl3RightChild6SVG.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 export default class TreeLvl3RightChild6SVG extends Component {
+  selectLvl3RightChild6 () {
+    console.log('click lvl 3 right child 6')
+  }
 
   render() {
-    return  <svg>
+    return  <svg onClick={this.selectLvl3RightChild6}>
               <line className="left-child-line" x1="377" y1="300" x2="345" y2="218" stroke="#F3C7C2" strokeWidth="8" />
               <circle cx="377" cy="300" r="18" stroke="white" strokeWidth="4" fill="#DA523C" />
             </svg>
--- public/Tree/TreeSVG/TreeLvl3RightChild8SVG.js
+++ public/Tree/TreeSVG/TreeLvl3RightChild8SVG.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 export default class TreeLvl3RightChild8SVG extends Component {
+  selectLvl3RightChild8 () {
+    console.log('click lvl 3 right child 8');
+  }
 
   render() {
-    return  <svg>
+    return  <svg onClick={this.selectLvl3RightChild8}>
               <line className="left-child-line" x1="509" y1="300" x2="475" y2="218" stroke="#F3C7C2" strokeWidth="8" />
               <circle cx="509" cy="300" r="18" stroke="white" strokeWidth="4" fill="#DA523C" />
             </svg>
