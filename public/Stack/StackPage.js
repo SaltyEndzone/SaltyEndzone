cfbbedaf999e49207492f906951eefc0b4aa49b4
Justin Bell <justinbell714@gmail.com>
2016-04-11 15:02:43 -0700

    Child components are separated in to relevant page sections

--- public/Stack/StackPage.js
+++ public/Stack/StackPage.js
@@ -3,11 +3,6 @@ import React, { Component } from 'react';
 import Stack from '../Stack/Stack';
 import StackDiscussion from './StackDiscussion';
 import StackCode from './StackCode';
-<<<<<<< c3f265f73a0b4f72aebf47f6ea8c8d1201b1e8b6
-=======
->>>>>>> Local authentication strategy works. Separated out discussion components in to relevant containers.
-
->>>>>>> Local authentication strategy works. Separated out discussion components in to relevant containers.
 
 export default class StackPage extends Component {
   render() {
