2e5bfdc7f5c926e1c4219821009ebc34aa761f6a
Justin Bell <justinbell714@gmail.com>
2016-04-12 16:01:50 -0700

    Added files for discussion pages and authentication.

--- public/App/App.js
+++ public/App/App.js
@@ -7,10 +7,6 @@ import { openLogin, closeLogin }  from '../Login/LoginActions';
 
 
 
-<<<<<<< 50fcdf9fd3e73f598f4c97628eacd1172ffbc637
-=======
-
->>>>>>> Local authentication strategy works. Separated out discussion components in to relevant containers.
 export default class App extends Component {  
   constructor(props) {
     super(props);
